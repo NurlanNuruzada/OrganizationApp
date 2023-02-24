d576becd541169fdad500a749b8d1f813d298478
unknown <nurlangn@code.edu.az>
2023-02-25 00:38:31 +0400

    ededin_sade_murekkeb_olub-olmamasi

---
+++ Practice/murekkebEdedler/murekkebEdedler/Program.cs
@@ -0,0 +1,18 @@
+﻿Console.WriteLine("eded daxil edin");
+int eded = int.Parse(Console.ReadLine());
+bool murekkeb = false;
+for (int i = 2;i<eded; i++)
+{
+    if (eded % i == 0)
+    {
+        murekkeb = true;
+    }  
+}
+if (murekkeb)
+{
+        Console.WriteLine("eded murekkebdir");
+}
+else
+{
+    Console.WriteLine("eded sadedir");
+}
