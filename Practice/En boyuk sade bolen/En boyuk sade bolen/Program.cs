f8f2624d54049b3a859ee3579d7d15fa89b7226c
unknown <nurlangn@code.edu.az>
2023-02-25 19:40:20 +0400

    practice-5

---
+++ Practice/En boyuk sade bolen/En boyuk sade bolen/Program.cs
@@ -0,0 +1,47 @@
+﻿
+using System.Globalization;
+Console.WriteLine("enter number");
+long number = long.Parse(Console.ReadLine());
+for (long i = number; 1 < i; i--)
+{
+    if (number % i == 0)
+    {
+        bool test = false;
+        for (int j = 2; j < i; j++)
+        {
+            if (i % j == 0)
+                test = true;
+            continue;
+        }
+        if (!test)
+        {
+            Console.WriteLine(i);
+        }
+    }
+}
+
+/* sade while
+ * int j = 2;
+int i = 18;
+bool sade = false;
+while (j < i-1)
+{
+    j++;
+    if (i % j == 0)
+    {
+        sade = true;
+    }
+}
+if (sade)
+{
+        Console.WriteLine("murekkeb");
+}
+else
+{
+        Console.WriteLine("sade");
+}
+
+*/
+/*sade
+
+*/
