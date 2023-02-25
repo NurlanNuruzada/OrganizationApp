80e840585ff00b42318c29283d4f48e5f7e12313
unknown <nurlangn@code.edu.az>
2023-02-25 17:49:50 +0400

    fibonaci's_even_numbers_sum

---
+++ Practice/Even Fibonacci numbers/Even Fibonacci numbers/Program.cs
@@ -0,0 +1,19 @@
+﻿Console.WriteLine("INPUT THE NUMBER");
+int num = int.Parse(Console.ReadLine());
+int total = 0;
+int num1 = 1;
+int num2 = 1;
+int num3 = 2;
+while (num3<num)
+{
+    num3 = num1 + num2;
+    num1 = num2;
+    num2 = num3;
+    if (num1 % 2 == 0)
+    {
+        total += num1;
+    }
+}
+Console.WriteLine($"sum of Fibonacci's even numbers smaller than input  number is :");
+Console.WriteLine(total);
+
