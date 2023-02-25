a8de0f4ade37ed13b252516bdd58b067e019d89e
unknown <nurlangn@code.edu.az>
2023-02-25 15:38:44 +0400

    Find_the_sum_of_all_the_multiples_of_3_or_5_below_number

---
+++ Practice/ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,11 @@
+﻿Console.WriteLine("enter number :");
+int number = int.Parse(Console.ReadLine());
+int sum = 0;
+for (int i = 0; i <number; i++)
+{
+    if (i % 3 == 0 | i % 5 == 0)
+    {
+        sum += i;
+    }
+}
+Console.WriteLine(sum);
