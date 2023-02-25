46870a2038270238bb3a8c053061798b256cd1ee
unknown <nurlangn@code.edu.az>
2023-02-25 14:40:05 +0400

    Her_hansi_ededin_quvveti

---
+++ Practice/4 un quvveti olub olma-olmama/4 un quvveti olub olma-olmama/Program.cs
@@ -0,0 +1,18 @@
+﻿Console.WriteLine("inut pow number");
+int pow = int.Parse(Console.ReadLine());
+Console.WriteLine("input test number");
+int number = int.Parse(Console.ReadLine()); ;
+bool test = false;
+for (int i = 1; i <= number; i*=pow)
+{
+    if (number == i)
+    {
+        test = true;
+        Console.WriteLine($"the number {number} is pow of {pow}");
+        break;
+    }
+}
+    if (!test)
+    {
+        Console.WriteLine($"the number {number} is not pow of {pow}");
+    }
