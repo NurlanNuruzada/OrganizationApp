e5bf88af9c2e7a2091e84cdad85231bd3df68836
unknown <nurlangn@code.edu.az>
2023-03-15 07:00:38 +0400

    added homework

---
+++ Hometask/implict/Hometask2/Person.cs
@@ -0,0 +1,16 @@
+﻿namespace Hometask2;
+
+public class Person
+{
+    public string mane { get; set; }
+    public string surmane { get; set; }
+    public int Age { get; set; }
+
+    public static bool operator >(Person p1, Person p2)
+    {
+        return p1.Age > p2.Age;
+    }    public static bool operator <(Person p1, Person p2)
+    {
+        return p1.Age < p2.Age;
+    }
+}
---
+++ Hometask/implict/Hometask2/Program.cs
@@ -0,0 +1,17 @@
+﻿using Hometask2;
+
+Person p1= new Person();
+p1.Age=7;
+Person p2= new Person();
+p2.Age = 2;
+Person[] arr = {p1,p2};
+    int i = 0;
+    int j = i + 1;
+if (arr[j] < arr[i])
+{
+    Person temp = arr[i];
+    arr[i] = arr[j];
+    arr[j] = temp;
+}
+foreach (Person p in arr)
+    Console.WriteLine(p.Age);
